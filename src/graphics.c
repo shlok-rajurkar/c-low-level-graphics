3990932bb866dc81186bdccbf3ab23490898b78f
Shlok Rajurkar <228423547+shlok-rajurkar@users.noreply.github.com>
2026-09-10 09:31:46 -0700

    Implement put pixel

--- src/graphics.c
+++ src/graphics.c
@@ -7,31 +7,95 @@
 
 uint32_t framebuffer[WIDTH * HEIGHT]; 
 
+void put_pixel(int x, int y, uint32_t color) {
+    framebuffer[WIDTH * y  + x] = color;
+}
+
 int main(void) {
+
     SDL_Window *window;
     SDL_Renderer *renderer;
     SDL_Texture *texture;
+    SDL_Event event;
     
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        fprintf(stderr, "SDL Init Failed: %s \n", SDL_GetError());
+        return EXIT_FAILURE;
+    };
+
+
     window = SDL_CreateWindow(
         "SDL Framebuffer", 
-        WIDTH, 
-        HEIGHT, 
+        WIDTH * 4, 
+        HEIGHT * 4, 
         0
     );
+
+    if (window == NULL) {
+        fprintf(stderr, "SDL_CreateWindow failed: %s \n", SDL_GetError());
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
 
     renderer = SDL_CreateRenderer(
         window,
         NULL
     );
 
+    if (renderer == NULL) {
+        fprintf(stderr, "SDL_CreateRenderer failed: %s \n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
 
     texture = SDL_CreateTexture(
         renderer,
         SDL_PIXELFORMAT_XRGB8888,
         SDL_TEXTUREACCESS_STREAMING,
-        WIDTH,
+        WIDTH, 
         HEIGHT
     );
-    SDL_Init(SDL_INIT_VIDEO);
+
+    if (texture == NULL) {
+        fprintf(stderr, "SDL_CreateTexture failed: %s \n", SDL_GetError());
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+
+    uint8_t is_running = 1;
+
+    while (is_running) {
+
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                is_running = 0;
+            }
+        }
+
+
+
+        put_pixel(WIDTH/2, HEIGHT/2, 0x00FF00);
+
+
+
+
+        SDL_UpdateTexture(texture, NULL, framebuffer, WIDTH * sizeof(uint32_t));
+
+        SDL_RenderClear(renderer);
+        SDL_RenderTexture(renderer, texture, NULL, NULL);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+
     return EXIT_SUCCESS;
 }
+
+
